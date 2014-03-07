3b97f6e75dc32154026a166a1934b1adb282979d
jbaker10 <jer.baker10@gmail.com>
2014-03-07 14:09:18 -0500

    mount test

---
+++ mount.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  mount.sh
+#  
+#
+#  Created by Jeremiah Baker on 1/16/14.
+#
+mkdir /Volumes/Keys
+mount_afp "afp://sysadmin:8065deanadmin@imaging.dco-intranet.lan/repo/" /Volumes/Keys
