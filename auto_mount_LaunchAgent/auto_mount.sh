3becdfccfdfe06bf8b5f2bf511e13b6a20bf90fa
jbaker10 <jer.baker10@gmail.com>
2014-05-19 08:54:39 -0400

    Changed directory name, added to the mount script

--- auto_mount_LaunchAgent/auto_mount.sh
+++ auto_mount_LaunchAgent/auto_mount.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -xvf
 
 #  auto_mount.sh
 #  
@@ -16,49 +16,58 @@ dir4=/Volumes/DOCS
 
 ## The mount variables are where you specify the different shares you want to mount, this can be as many as you want
 
-mount1=`mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Users/jeremiah.baker /Volumes/jeremiah.baker`
-mount2=`mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Shared /Volumes/Shared`
-mount3=`mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Shared/IT/Private /Volumes/Private`
-mount4=`mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Shared/IT/Private/DOCS /Volumes/DOCS`
+mount1="/sbin/mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Users/jeremiah.baker /Volumes/jeremiah.baker"
+mount2="/sbin/mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Shared /Volumes/Shared"
+mount3="/sbin/mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Shared/IT/Private /Volumes/Private"
+mount4="/sbin/mount_smbfs //jeremiah.baker@va1srvgenfs01.dco-intranet.lan/Shared/IT/Private/DOCS /Volumes/DOCS"
 
 
 ## Sets a while loop so if it isn't on the network, it tries for 1 minute by pinging an internal resource
 
 i=1
-while [ $i -lt 12 ]
+while [ $i -lt 12 ];
 do
     i=`expr $i + 1`
-    if ping -q -c dco-intranet.lan; then
+    if ping -q -c 1 dco-intranet.lan; then
         if mount | grep $dir1 > /dev/null; then
             echo "$dir1 is already mounted"
         else
             echo "Share not mounted"
-            mounts[$[${#mounts[@]}+1]]=mount1
+            mkdir $dir1
+            mounts[$[${#mounts[@]}+1]]=$mount1
         fi
         if mount | grep $dir2 > /dev/null; then
             echo "$dir2 is already mounted"
         else
-            echo "Share already mounted"
-            mounts[$[${#mounts[@]}+1]]=mount2
+            echo "Share not mounted"
+            mkdir $dir2
+            mounts[$[${#mounts[@]}+1]]=$mount2
         fi
         if mount | grep $dir3 > /dev/null; then
             echo "$dir3 is already mounted"
         else
-            echo "Share already mounted"
-            mounts[$[${#mounts[@]}+1]]=mount3
+            echo "Share not mounted"
+            mkdir $dir3
+            mounts[$[${#mounts[@]}+1]]=$mount3
         fi
         if mount | grep $dir4 > /dev/null; then
             echo "$dir4 is already mounted"
         else
-            echo "Share already mounted"
-            mounts[$[${#mounts[@]}+1]]=mount4
+            echo "Share not mounted"
+            mkdir $dir4
+            mounts[$[${#mounts[@]}+1]]=$mount4
         fi
-echo ${mounts[@]}
+        break;
+    fi
+done
+echo ${mounts[@]};
 
-count=0
-for i in ${mounts[@]}; do
-    mounts[$count];
-    ((count++))
+count=1
+for i in ${mounts[@]};
+do
+    ${mounts[$count]};
+    echo $count;
+    ((count++));
 done
 
 #mount_drives(){
@@ -71,4 +80,5 @@ done
 #        echo "Could not ping internal resource"
 #    fi
 #mount_drives($j)
-done
+#done
+exit 0;
