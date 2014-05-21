2ae02c8453d10f5a688ea2d7b6e298e5fd1e8aa9
jbaker10 <jer.baker10@gmail.com>
2014-05-21 15:50:07 -0400

    Moved files into more specific repos

--- specs.sh
+++
@@ -1,8 +0,0 @@
-modelid=`sysctl hw.model`
-processor=`system_profiler | grep Processor`
-ram=`system_profiler | grep Memory | grep GB`
-hdd=`system_profiler | grep Capacity | grep GB | cut -d ':' -f2`
-
-echo "Your processor information is: $processor"
-echo "Your Memory information is: $ram"
-echo "Your Storage information is: $hdd"
